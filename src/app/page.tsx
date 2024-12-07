98330f0ca4059287ab72f7bd19037972ecd98d72
kainaturooj <128933130+kainaturooj@users.noreply.github.com>
2024-12-07 20:28:20 +0500

    Your commit message describing the changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,53 @@ export default function Home(){
   return(
     <>
     <h1>this is home page</h1>
+
+    {/* ///// testing the gpt code ////////// */}
     
+
+    {/* <body className="bg-[#212529]">
+  <div className="mainContainer h-[70px] w-[1366px]">
+    <div className="rightContainer h-[60px] w-[400px] text-[#e5ad06] ml-[50px] mt-[20px]"> */}
+      {/* <!-- Right container content --> */}
+    {/* </div>
+  </div>
+
+  <div className="homeContainer mt-[50px]">
+    <h1 className="text-base font-inherit">Your Heading</h1>
+
+    <ul className="flex text-center font-bold text-[1.3rem]">
+      <li className="hover:underline hover:text-[#f3eaea]">Item 1</li>
+      <li className="hover:underline hover:text-[#f3eaea]">Item 2</li>
+      <li className="hover:underline hover:text-[#f3eaea] active:underline">Item 3</li>
+    </ul>
+
+    <h2 className="greetHeading font-inherit text-center font-bold italic text-shadow-[1px_1px_2px_#404243]"> */}
+      {/* Welcome!
+    </h2>
+  </div>
+</body> */}
+
+{/* ////////////// testing od responsive code ........ */}
+
+     <div className=" bg-slate-500 flex justify-center my-11
+      sm:w-full sm:bg-re-400 sm:flex-co
+      md:w-[80%] md:bg-gree-400  md:flex-row">
+                {/* Left Section */}
+                <div className="bg-blue-700
+                sm:w-full sm:p-8 md:p-0
+                 md:w-[60%] ">
+                    <h1 className=" font-bold
+                    sm:text-xl 
+                    md:text-3xl ">Our Story</h1>
+                    <p className="pt-6 bg-yellow-400">
+                        Launched in 2015, Exclusive is South Asia&apos;s premier online shopping marketplace with an active presence in Bangladesh. Supported by a wide range of tailored marketing, data, and service solutions, Exclusive has 10,500 sellers and 300 brands and serves 3 million customers across the region.
+                    </p>
+                    <p className="mt-5 bg-yellow-950">
+                        Exclusive has more than 1 million products to offer, growing at a very fast pace. Exclusive offers a diverse assortment in categories ranging from consumer goods.
+                    </p>
+                </div>
+                </div>
+
     
     
     
